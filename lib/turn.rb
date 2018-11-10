e958bfe2279240726d4b7c02c58c6dc4e0f95058
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-10 18:34:19 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,6 @@
+def valid_move?(board,index)
+  index.between?(0, 8) && !position_taken?(board, index)
+end
+def position_taken?(board, index)
+     board[index] != " " && board[index] != "" && board[index] != nil
+end 
